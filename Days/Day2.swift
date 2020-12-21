df448ffb85eef04b526ebe2cec5443f447dcec3f
Simon Deutsch <>
2020-12-21 20:10:27 +0100

    day2 part 2 (beautiful tho!)

--- Days/Day2.swift
+++ Days/Day2.swift
@@ -13,14 +13,14 @@ class Day2: Day {
         
         var correctPWs = 0
         data.forEach { line in
-            if isPasswordValid(line: line) {
+            if isPasswordValid(line: line, company: .TobogganCorporate) {
                 correctPWs+=1
             }
         }
         print(correctPWs)
     }
 //    1-3 a: abcde
-    func isPasswordValid(line: String) -> Bool {
+    func isPasswordValid(line: String, company: Company) -> Bool {
         if line.isEmpty { return false }
         
 //       [0] 1
@@ -38,9 +38,33 @@ class Day2: Day {
             return false
             
         }
-        let ruleChar = split2[1].first
-        let password = split2[2]
+        guard let ruleChar = split2[1].first else {
+            return false
+        }
         
+        let password = String(split2[2])
+        
+        return company.rule(password: password, ruleChar: ruleChar, min: min, max: max)
+    }
+}
+
+enum Company {
+    case TobogganCorporate
+    case SledRental
+}
+
+extension Company {
+    
+    func rule(password: String, ruleChar: Character, min: Int, max: Int) -> Bool {
+        switch self {
+        case .TobogganCorporate:
+            return TobogganCorporateRule(password: password, ruleChar: ruleChar, min: min, max: max)
+        case .SledRental:
+            return SledRentalRule(password: password, ruleChar: ruleChar, min: min, max: max)
+        }
+    }
+    
+    private func SledRentalRule(password: String, ruleChar: Character, min: Int, max: Int) -> Bool {
         var occurences = 0
         password.forEach{ char in
             if char == ruleChar {
@@ -50,4 +74,20 @@ class Day2: Day {
         
         return occurences >= min && occurences <= max
     }
+
+    private func TobogganCorporateRule(password: String, ruleChar: Character, min: Int, max: Int) -> Bool {
+        if password.count < max { return false }
+        
+        
+        let check1 = Array(password)[min - 1] == ruleChar
+        let check2 = Array(password)[max - 1] == ruleChar
+        
+        switch (check1, check2) {
+        case (false, false), (true, true):
+            return false
+        case (true, false), (false, true):
+            return true
+        }
+    }
 }
+
