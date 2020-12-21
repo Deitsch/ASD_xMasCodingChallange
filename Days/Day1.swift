b9c349601a6b72d181ed7feb683db905fedf8438
Simon Deutsch <>
2020-12-21 18:42:41 +0100

    lazy day1 part 2

--- Days/Day1.swift
+++ Days/Day1.swift
@@ -10,14 +10,28 @@ import Foundation
 class Day1: Day {
     func run() {
         let data = FileReader.getStringArray(from: "/Users/simondeutsch/Documents/Campus/ASD/ASD_xMasCodingChallenge/Ressources/data.txt").toIntArray()
-        print(find2020(data: data) ?? "fail")
+        print(find2020by3(data: data) ?? "fail")
     }
     
-    func find2020(data: [Int]) -> Int? {
+    func find2020(data: [Int], cnt: String) -> Int? {
         for n1 in data {
             for n2 in data {
                 if  n1 + n2 == 2020 {
                     return n1 * n2
+                }
+            }
+        }
+        return nil
+    }
+    
+    // lazy solution
+    func find2020by3(data: [Int]) -> Int? {
+        for n1 in data {
+            for n2 in data {
+                for n3 in data {
+                    if  n1 + n2 + n3 == 2020 {
+                        return n1 * n2 * n3
+                    }
                 }
             }
         }
