7706a120947b6da246ec938618987e5645c9ea37
Simon Deutsch <>
2020-12-21 21:14:18 +0100

    day3 part 2

--- Days/Day3.swift
+++ Days/Day3.swift
@@ -11,16 +11,44 @@ class Airplane {
     var x: Int
     var y: Int
     
-    init(x: Int, y: Int) {
+    private var movementPattern: AirplaneMovementPattern
+    
+    init(x: Int, y: Int, pattern: AirplaneMovementPattern) {
         self.x = x
         self.y = y
+        self.movementPattern = pattern
     }
     
     func nextPosition() {
-        x+=3
-        y+=1
+        (x, y) = movementPattern.nextPosition(x: x, y: y)
     }
 }
+
+enum AirplaneMovementPattern: CaseIterable {
+    case r1d1
+    case r3d1
+    case r7d1
+    case r1d2
+    case r5d1
+}
+
+extension AirplaneMovementPattern {
+    func nextPosition(x: Int, y: Int) -> (Int, Int) {
+        switch self {
+        case .r1d1:
+            return (x+1, y+1)
+        case .r3d1:
+            return (x+3, y+1)
+        case .r7d1:
+            return (x+7, y+1)
+        case .r1d2:
+            return (x+1, y+2)
+        case .r5d1:
+            return (x+5, y+1)
+        }
+    }
+}
+
 
 class Day3: Day {
     
@@ -28,23 +56,30 @@ class Day3: Day {
     var mapHeight: Int?
     
     func run() {
-        let data = FileReader.getStringArray(from: basePath + "/Ressources/day3_input.txt")
-        mapWidth = data.first?.count
-        mapHeight = data.count
+        let map = FileReader.getStringArray(from: basePath + "/Ressources/day3_input.txt")
+        mapWidth = map.first?.count
+        mapHeight = map.count
         
-        let plane = Airplane(x: 0, y: 0)
         
+        var multipliedCollisions = 1
+        AirplaneMovementPattern.allCases.forEach { pattern in
+            let plane = Airplane(x: 0, y: 0, pattern: pattern)
+            multipliedCollisions *= getCollisions(plane: plane, map: map)
+        }
+        print(multipliedCollisions)
+    }
+    
+    func getCollisions(plane: Airplane, map: [String]) -> Int {
         var collisions = 0
         
         while plane.y < mapHeight! - 1 {
             print(plane.x, plane.y)
-            if didCollide(plane: plane, map: data) {
+            if didCollide(plane: plane, map: map) {
                 collisions+=1
             }
             plane.nextPosition()
         }
-        
-        print(collisions)
+        return collisions
     }
     
     func didCollide(plane: Airplane, map: [String]) -> Bool {
