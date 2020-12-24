3f1dc331028ac185e13c46e93beda062e2cb7ebf
Simon Deutsch <>
2020-12-24 16:09:47 +0100

    day5 part2

--- Days/Day5.swift
+++ Days/Day5.swift
@@ -7,19 +7,60 @@
 
 import Foundation
 
+typealias SeatMap = [[Int]]
+
 class Day5: Day {
     func run() {
         let data = FileReader.getStringArray(from: basePath + "Resources/day5_input.txt")
-    
+        
         var highestSeatID = 0
+        var lowestSeatID = Int.max
+        var seatIDs = [Int]()
         data.forEach{ line in
             if !line.isEmpty {
                 let seatID = calculateSeatID(line: line)
+                seatIDs.append(seatID)
                 highestSeatID = seatID > highestSeatID ? seatID : highestSeatID
+                lowestSeatID = seatID < lowestSeatID ? seatID : lowestSeatID
             }
         }
         
         print("Part1", highestSeatID)
+        
+        let seatMap = calculateAllPossibleSeatIDs(cols: 8, rows: 128)
+        
+        
+        let reducedSeatMap = removeRowsBetween(lowerbound: lowestSeatID, upperbound: highestSeatID, in: seatMap).flatMap { $0 }
+        
+        let possibleSeats = Set(reducedSeatMap).symmetricDifference(Set(seatIDs))
+        
+        possibleSeats.forEach { seatID in
+            if seatIDs.contains(seatID + 1) && seatIDs.contains(seatID - 1) {
+                print("Part2", seatID)
+            }
+        }
+    }
+    
+    
+    func removeRowsBetween(lowerbound: Int, upperbound: Int, in map: SeatMap) -> SeatMap {
+        var removeCnt = 0
+        for col in map {
+            if col.contains(lowerbound) {
+                break
+            }
+            removeCnt+=1
+        }
+        let removedFirst = map.dropFirst(removeCnt)
+        
+        
+        removeCnt = 0
+        for col in removedFirst.reversed() {
+            if col.contains(upperbound) {
+                break
+            }
+            removeCnt+=1
+        }
+        return removedFirst.dropLast(removeCnt)
     }
     
     func calculateSeatID(line: String) -> Int {
@@ -31,9 +72,12 @@ class Day5: Day {
         let row = calculateSeatRow(rowString: rowCode, lowerBound: 0, upperBound: 127)
         let col = calculateSeatColumn(colString: colCode, lowerBound: 0, upperBound: 7)
         
-        return row * 8 + col
+        return calculateSeatId(col: col, row: row)
     }
     
+    func calculateSeatId(col: Int, row: Int) -> Int {
+        return row * 8 + col
+    }
     
     // calcSeat and calcRow are almost the same -> could be 1 function with bounderies identifier
     func calculateSeatRow(rowString: String, lowerBound: Int, upperBound: Int) -> Int {
@@ -62,5 +106,17 @@ class Day5: Day {
         let newColString = String(colString.dropFirst())
         
         return calculateSeatColumn(colString: newColString, lowerBound: newLowerBound, upperBound: newUpperBound)
+    }
+    
+    func calculateAllPossibleSeatIDs(cols: Int, rows: Int) -> SeatMap{
+        var seatMap = SeatMap()
+        
+        for row in 0..<rows {
+            seatMap.append([Int]())
+            for col in 0..<cols {
+                seatMap[row].append(calculateSeatId(col: col, row: row))
+            }
+        }
+        return seatMap
     }
 }
